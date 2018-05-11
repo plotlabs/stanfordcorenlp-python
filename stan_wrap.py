be36caec5e8a7dd92846c058317a0eb8923f1c3a
Vedanshi Kataria <vedanshikataria@Vedanshis-MacBook-Pro.local>
2018-05-11 15:08:42 +0530

    Problems loading Stanford Jar

--- stan_wrap.py
+++ stan_wrap.py
@@ -1,21 +1,31 @@
 import jpype
 from jpype import *
 
-#jpype.startJVM(jpype.getDefaultJVMPath())
 jvmPath = jpype.getDefaultJVMPath() 
-print jvmPath
 #jpype.startJVM(jvmPath, "-ea", "-Djava.class.path=/Users/vedanshikataria/Documents/practice_python/PlotLabs/wrapper/stanford-corenlp-3.9.1.jar")
-jpype.startJVM(jvmPath, "-Djava.class.path=stanford-corenlp-3.9.1")
+#classpath = "stanford-corenlp-3.9.1.jar"
+#jpype.startJVM(jvmPath, "-Djava.class.path = %s" % classpath)
+jpype.startJVM(jvmPath, "-Djava.class.path=stanford-corenlp-3.9.1.jar")
+jpype.java.lang.System.out.println("hello world")
 
-#jpype.java.lang.System.out.println("hello world")
+systemclassloader = jpype.java.lang.ClassLoader.getSystemClassLoader()
+for x in systemclassloader.getURLs():
+    print x ,"\n"
+    print x.path
+    print x.getRef
 
-# stanPkg = jpype.JPackage('edu').stanford.nlp.naturalli
-# print stanPkg
-ie_obj = jpype.JClass('edu').stanford.nlp.naturalli
-get_ie = ie_obj.OpenIEDemo.main()
+arrobj = jpype.JPackage("java").util
+print arrobj
+x = arrobj.ArrayList
+nar = x()
+print dir(nar)
+print nar
 
-#try = jpype.JPackage('edu').stanford.nlp.naturalli.JpypeTrial
-#jp_trial = try.call_try()
-#print jp_trial
+#---------------------------------------------------------------------------------------------------------
+
+ie_obj = jpype.JPackage("edu.stanford.nlp.naturalli")
+print dir(ie_obj)
+w = ie_obj.JpypeTrial
+print w().call_try()
 
 jpype.shutdownJVM()
